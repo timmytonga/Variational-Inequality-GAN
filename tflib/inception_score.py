14e3bae67fac5e82e44fb3fa1a45c4765c881140
Thien <thienhn4@uci.edu>
2020-11-21 16:20:00 -0500

    exp

--- tflib/inception_score.py
+++ tflib/inception_score.py
@@ -76,7 +76,7 @@ def _init_inception():
     statinfo = os.stat(filepath)
     print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
   tarfile.open(filepath, 'r:gz').extractall(MODEL_DIR)
-  with tf.gfile.FastGFile(os.path.join(
+  with tf.io.gfile.FastGFile(os.path.join(
       MODEL_DIR, 'classify_image_graph_def.pb'), 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
